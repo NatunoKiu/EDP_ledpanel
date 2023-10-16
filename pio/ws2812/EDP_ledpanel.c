93fcbf10f985d7a3b0b91ca8cabb2bd762f2d864
NatunoKiu <natukiu7272@gmail.com>
2023-10-16 14:30:03 +0900

    EDPで使うws2812のプログラムのファイル名を変更。pattern_EDPに文字データを追加。hexToBinに引数の要素数取得処理を追加。

--- pio/ws2812/EDP_ledpanel.c
+++ pio/ws2812/EDP_ledpanel.c
@@ -26,21 +26,43 @@
 
 
 /*
-hexをbinに変換する関数hexToBin
-1つのhexをbinに変換して戻り値で１つのbinを返す
-16進数の各桁を１０進数に変換
+２桁のhexをbinに変換する関数hexToBin
+1. 1つのhexをbinに変換
+2. 戻り値で１つのbinを返す
+
+16進数の各桁を１０進数に変換　strtolで
 それを２で割って
+
+char型をint型にキャストするとchar型の文字に割り当てられてる文字コードの数字に変換される。
+    -> ex. 'A' だったら、'65'
+
+1. hexをcharにキャスト
+2. charの配列の要素を指定して、hexの各桁のみを取り出す。
+3. 取り出したhexの値が、0...9...A...F なら dexの 0...9...10...15 に変換する。
+    1. 取り出した各桁を int型 にキャスト
+        2.(0-9) '0' = 48
+        2.(A-F) 'A' = 65
+    2. キャストした値から48を引く
+    3. 2.の値が9より大きいなら
+        65-48-7 を引く
+
 
 １０進数を２で割った余りを使う方法
 １０進数を４bitの２進数の各桁が示す１０進数の値の内大きい順から割っていく方法
-
 */
-uint hexToBin(char hex){
-    const uint bin4bitdec[4] = {8, 4, 2, 1};
-    
-    
-    return ;
+
+
+char hexToBin(uint32_t hex){
+    uint8_t hexlen = sizeo((char)hex) / sizeof((char)hex[0]);
+    printf('hexlen:'%d,hexlen);
+    for( i=0; i < hexlen; i++ ){
+        char bin = strtol((char)hex, NULL, 16);
+    }
+
+    printf('binnum:'%d,bin);
+    return bin;
 }
+
 
 static inline void put_pixel(uint32_t pixel_grb) {
     pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
@@ -53,19 +75,19 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
             (uint32_t) (b);
 }
 
-void framebuffer(uint len, uint t) {
+void framebuffer() {
     for(uint i=0; i<128; i++){
         put_pixel(urgb_u32(0x00, 0x00, 0x10));
     }
 }
-
-/*  
+  
 void pattern_EDP(uint len, uint t) {
+    //文字データ：新潟大学コンピュータクラブ  -- EDP --
+    uint8_t print_char[] = { 0xf2, 0x00,0x01,0x02,0x03, 0xf0, 0xa8,0xe8,0xc9,0xda,0xee,0xb4,0xa4,0xde,0xcb, 0xf1, 0x00,0x00,0x0d,0x0d,0x00,0x25,0x24,0x30,0x00,0x0d,0x0d, };
     for (uint i = 0; i < len; ++i) {
         put_pixel(urgb_u32(0, 0, 0x20));
     }
 }
-*/
 
 void pattern_snakes(uint len, uint t) {
     for (uint i = 0; i < len; ++i) {
@@ -110,12 +132,11 @@ const struct {
     const char *name;
 } pattern_table[] = {
     //    {pattern_EDP,     "EDP"},
-        {framebuffer,    "test"}
-        /*{pattern_snakes,  "Snakes!"},
+        //{framebuffer,    "test"}
+        {pattern_snakes,  "Snakes!"},
         {pattern_random,  "Random data"},
         {pattern_sparkle, "Sparkles"},
-        {pattern_greys,   "Greys"},
-        */
+        {pattern_greys,   "Greys"},    
 };
 
 int main() {
